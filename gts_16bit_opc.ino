490977620d03125287dd717e4d39854852c71802
Polyneikis Tzanis <polyneikis.tzanis@cern.ch>
2018-06-08 18:16:31 +0000

    Replace gts_16bit_opc.ino

--- gts_16bit_opc.ino
+++ gts_16bit_opc.ino
@@ -21,24 +21,29 @@ const int LED=53;
 // create a callback function for the OPCItem
 
 float callbackA0(const char *itemID, const opcOperation opcOP, const float value){
-  return ads.readADC_SingleEnded(0)*0.0001875F;
+  return ads.readADC_SingleEnded(0);
 }
 
 float callbackA1(const char *itemID, const opcOperation opcOP, const float value){
-  return ads.readADC_SingleEnded(1)*0.0001875F;
+  return ads.readADC_SingleEnded(1);
 }
 
 float callbackA2(const char *itemID, const opcOperation opcOP, const float value){
-  return ads.readADC_SingleEnded(2)*0.0001875;
+  return ads.readADC_SingleEnded(2);
 }
 
 float callbackA3(const char *itemID, const opcOperation opcOP, const float value){
-  return ads.readADC_SingleEnded(2)*0.0001875F;
+  return ads.readADC_SingleEnded(3);
 }
 
 
 
   
+
+
+void setup() {
+
+  pinMode(LED,OUTPUT); 
   // The ADC input range (or gain) can be changed via the following
   // functions, but be careful never to exceed VDD +0.3V max, or to
   // exceed the upper and lower limits if you adjust the input range!
@@ -46,16 +51,11 @@ float callbackA3(const char *itemID, const opcOperation opcOP, const float value
   //                                                                ADS1015  ADS1115
   //                                                                -------  -------
   // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
-  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+   ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
   // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
   // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
   // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-
-
-void setup() {
-
-  pinMode(LED,OUTPUT); 
   ads.begin();
 
    /*
